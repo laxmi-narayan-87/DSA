b4c858b0541cd28fca7c7eba2502395d56db8e5f
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-06-18 23:34:37 +0530

    0037-sudoku-solver 06-18-2026 Hard Array

---
+++ Python3/Hard/0037-sudoku-solver/0037-sudoku-solver-06-18-2026-23-34-35.py
@@ -0,0 +1,40 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        empty = []
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    empty.append((r, c))
+                else:
+                    val = board[r][c]
+                    rows[r].add(val)
+                    cols[c].add(val)
+                    boxes[(r // 3) * 3 + (c // 3)].add(val)
+
+        def backtrack(index):
+            if index == len(empty):
+                return True
+            r, c = empty[index]
+            box = (r // 3) * 3 + (c // 3)
+            for num in '123456789':
+                if (num not in rows[r] and
+                    num not in cols[c] and
+                    num not in boxes[box]):
+                    board[r][c] = num
+                    rows[r].add(num)
+                    cols[c].add(num)
+                    boxes[box].add(num)
+                    if backtrack(index + 1):
+                        return True
+                    board[r][c] = '.'
+                    rows[r].remove(num)
+                    cols[c].remove(num)
+                    boxes[box].remove(num)
+            return False
+        backtrack(0)
