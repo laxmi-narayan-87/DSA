744d82f8d0e50c2e44a56f837d0d20d5e7461a89
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-07-05 23:42:00 +0530

    0987-vertical-order-traversal-of-a-binary-tree 07-05-2026 Hard Hash Table

---
+++ Python3/Hard/0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree-07-05-2026-23-41-58.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        nodes = []
+        def dfs(node, row, col):
+            if not node:
+                return
+            nodes.append((col, row, node.val))
+            dfs(node.left, row + 1, col - 1)
+            dfs(node.right, row + 1, col + 1)
+        dfs(root, 0, 0)
+        nodes.sort()
+        ans = []
+        prev_col = float('-inf')
+        for col, row, val in nodes:
+            if col != prev_col:
+                ans.append([])
+                prev_col = col
+            ans[-1].append(val)
+        return ans
