115a6bfd8315d249df288a4f17794b6f30a3b35f
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-07-20 18:11:06 +0530

    ✅ SALARY1 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-20

--- codechef/medium/SALARY1/solution.py
+++ codechef/medium/SALARY1/solution.py
@@ -4,6 +4,11 @@ def earning(s,b,r):
     total = s+b*r
     return total
 
-if __name__=="__main__":
+if __name__=="__main1__":
     s,b,r= map(int,input().split())
     print(earning(s,b,r))
+    
+if __name__=="__main__":
+    s,b,r=map(int,input().split())
+    total=s+b*r
+    print(total)
