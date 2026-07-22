574fa7dc5742eeeff0e2fd8b84fb3d4c0346744a
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-07-22 18:09:55 +0530

    ✅ JASSIGNMENTS - Rating 513 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-22

---
+++ codechef/easy/JASSIGNMENTS---Rating-513/solution.py
@@ -0,0 +1,9 @@
+# cook your dish here
+if __name__=="__main__":
+    t=int(input())
+    for _  in range(t):
+        x=int(input())
+        if x+3<=10:
+            print("yes")
+        else:
+            print("NO")
