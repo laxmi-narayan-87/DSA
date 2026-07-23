1c150b674f6801fb73d2f64df183cf49f11b85aa
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-07-23 09:54:21 +0530

    ✅ Bigger is Greater | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-23

---
+++ hackerrank/medium/bigger-is-greater/solution.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'biggerIsGreater' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING w as parameter.
+#
+
+def biggerIsGreater(w):
+    # Write your code here
+    w = list(w)
+    n = len(w)
+    i = n - 2
+    while i >= 0 and w[i] >= w[i + 1]:
+        i -= 1
+    if i == -1:
+        return "no answer"
+    j = n - 1
+    while w[j] <= w[i]:
+        j -= 1
+    w[i], w[j] = w[j], w[i]
+    w[i + 1:] = reversed(w[i + 1:])
+    return "".join(w)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        w = input()
+
+        result = biggerIsGreater(w)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
