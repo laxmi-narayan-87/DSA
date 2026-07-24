82355b17d0d19f8fa0c6b0ee3e63251b25f717be
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-07-24 18:08:58 +0530

    ✅ PASSORFAIL - Rating 730 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-24

--- codechef/easy/PASSORFAIL---Rating-730/solution.py
+++ codechef/easy/PASSORFAIL---Rating-730/solution.py
@@ -1,6 +1,10 @@
 # cook your dish here
 def check(n,x,p):
-    
+    marks= 3*x +(-1)*(n-x)
+    if p<=marks:
+        return "PASS"
+    else:
+        return "FAIL"
 
 if __name__=="__main__":
     t=int(input())
