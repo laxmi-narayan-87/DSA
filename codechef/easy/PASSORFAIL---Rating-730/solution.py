ce13580c4ede3a641e2ce1f38311a6a1569cec2c
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-07-24 18:04:35 +0530

    ✅ PASSORFAIL - Rating 730 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-24

--- codechef/easy/PASSORFAIL---Rating-730/solution.py
+++ codechef/easy/PASSORFAIL---Rating-730/solution.py
@@ -1,11 +1,10 @@
 # cook your dish here
+def check(n,x,p):
+    
+
 if __name__=="__main__":
     t=int(input())
     for _ in range(t):
-        x,y,z=map(int,input().split())
-        if (x >= y and x <= z) or (x <= y and x >= z):
-            print(x)
-        elif (y >= x and y <= z) or (y <= x and y >= z):
-            print(y)
-        else:
-            print(z)
+        n,x,p=map(int,input().split())
+        res=check(n,x,p)
+        print(res)
