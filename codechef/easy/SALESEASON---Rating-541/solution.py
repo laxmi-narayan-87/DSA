e72d4a898376447b8e13e715e21e8867611a3d47
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-07-28 16:12:25 +0530

    ✅ SALESEASON - Rating 541 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-28

--- codechef/easy/SALESEASON---Rating-541/solution.py
+++ codechef/easy/SALESEASON---Rating-541/solution.py
@@ -2,8 +2,12 @@
 if __name__=="__main__":
     t=int(input())
     for _ in range(t):
-        b1,b2,b3=map(int,input().split())
-        if b1+b2+b3<2:
-            print("Water filling time")
+        x=int(input())
+        if x>5000:
+            print(x-500)
+        elif  5000>=x>1000:
+            print(x-100)
+        elif 1000>=x>100:
+            print(x-25)
         else:
-            print("Not now")
+            print(x)
