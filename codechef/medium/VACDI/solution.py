5b4a38c62b6c792cf0a2c192d44674a796d14f9c
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-07-28 16:05:09 +0530

    ✅ VACDI | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-28

--- codechef/medium/VACDI/solution.py
+++ codechef/medium/VACDI/solution.py
@@ -5,7 +5,12 @@ if __name__=="__main__":
     p=list(map(int,input().split()))
     v.sort()
     p.sort()
-    if v[n-1]>p[n-1]:
+    flag=True
+    for i in range(n):
+        if v[i]<p[i]:
+            flag=False
+            break
+    if flag:
         print("YES")
     else:
         print("NO")
