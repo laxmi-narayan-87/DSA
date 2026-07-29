f7ce4f5542eb867f8c8b6081f71c218c673ad7b6
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-07-29 12:08:20 +0530

    ✅ SUGARCANE - Rating 563 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/easy/SUGARCANE---Rating-563/solution.py
+++ codechef/easy/SUGARCANE---Rating-563/solution.py
@@ -4,5 +4,6 @@ if __name__=="__main__":
     for _ in range(t):
         n=int(input())
         n=n*50
-        expend=(n//5)+(n//5)+((3*n)//10)
-        print(n-expend)
+        # expend=(n//5)+(n//5)+((3*n)//10)
+        expend=(n*.2)+(n*.2)+(n*.3)
+        print(int(n-expend))
