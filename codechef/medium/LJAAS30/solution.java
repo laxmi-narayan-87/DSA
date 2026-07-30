a71a5c234fa8193a38060a6d9ee6c121573ce97d
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-07-30 18:18:22 +0530

    ✅ LJAAS30 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-30

---
+++ codechef/medium/LJAAS30/solution.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Your code goes here
+        double temp=25.5;
+        double kelvin = temp + 273;
+        System.out.println("Celsius -"+temp+"\nKelvin -"+kelvin);
+        
+    }
+}
