c049e80a84ce7165c93d6f0fea889c19b72622d7
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-07-30 19:20:52 +0530

    ✅ LJAAS59 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-30

--- codechef/medium/LJAAS59/solution.java
+++ codechef/medium/LJAAS59/solution.java
@@ -8,15 +8,39 @@ public class Main
         int num;
         num = scanner.nextInt(); // input first number
         // Check first number
-        
+        if (num>0){
+            System.out.println("Positive");
+        }
+        else if (num<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("Zero");
+        }
         
         num = scanner.nextInt(); // input second number
         // Check second number
-        
+        if (num>0){
+            System.out.println("Positive");
+        }
+        else if (num<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("Zero");
+        }
         
         num = scanner.nextInt(); // input third number
         // Check third number
-        
+        if (num>0){
+            System.out.println("Positive");
+        }
+        else if (num<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("Zero");
+        }
         
     }
 }
