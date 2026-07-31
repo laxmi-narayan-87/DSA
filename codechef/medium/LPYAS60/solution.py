4002217b4ef9df4bcb718629838a538ae8683cda
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-07-31 09:40:50 +0530

    ✅ LPYAS60 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-31

--- codechef/medium/LPYAS60/solution.py
+++ codechef/medium/LPYAS60/solution.py
@@ -1,8 +1,11 @@
 # cook your dish here
-n=int(input())
-if n>0:
-    print("Positive")
-elif n<0:
-    print("Negative")
-else:
-    print("Zero")
+marks=int(input())
+match marks:
+    case _ if marks>90:
+        print("A")
+    case _ if marks >70:
+        print("B")
+    case _ if marks >=40:
+        print("C")
+    case _:
+        print("F")
