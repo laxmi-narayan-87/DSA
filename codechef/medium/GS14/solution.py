9fbbeb9f4bdcd707ad851ed8d0aef563ecc087c9
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-07-31 16:54:02 +0530

    ✅ GS14 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-31

--- codechef/medium/GS14/solution.py
+++ codechef/medium/GS14/solution.py
@@ -1,8 +1,5 @@
-# accept the count of test cases given in the the 1st line
-t=int(input())
-
-# run a for loop for each test case
-# then for each test case input an integer and output it's negative
-for _ in range(t):
-    n=int(input())
-    print(n*-1)
+t = int(input())
+for i in range(t):
+    # take input and output the join using +
+    x=input()
+    print(x*2)
