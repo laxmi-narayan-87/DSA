19b2526ae6e402b60f07e601685883d4992024ba
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-07-31 16:53:25 +0530

    ✅ GS13 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-31

---
+++ codechef/medium/GS13/solution.py
@@ -0,0 +1,8 @@
+# accept the count of test cases given in the the 1st line
+t=int(input())
+
+# run a for loop for each test case
+# then for each test case input an integer and output it's negative
+for _ in range(t):
+    n=int(input())
+    print(n*-1)
