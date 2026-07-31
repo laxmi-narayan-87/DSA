13c449cb0e2af8013922a5ede264116cfea840c7
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-07-31 15:27:48 +0530

    ✅ LJAAS60 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-31

---
+++ codechef/medium/LJAAS60/solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		Scanner sc= new Scanner(System.in);
+		for (int i = 0; i < 3; i++) {
+		int marks=sc.nextInt();
+		if (marks>90){
+		    System.out.println("A");
+		}
+		else if (marks>70){
+		    System.out.println("B");
+		}
+		else if (marks>=40){
+		    System.out.println("C");
+		}
+		else{
+		    System.out.println("F");
+		    
+		}}
+		sc.close();
+	}
+}
