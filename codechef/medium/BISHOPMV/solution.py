5026dad67831385d2809aad1e9399cec1cd5e053
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-05 21:34:44 +0530

    ✅ BISHOPMV | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-05

---
+++ codechef/medium/BISHOPMV/solution.py
@@ -0,0 +1,12 @@
+# cook your dish here
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(t):
+        x1,y1,x2,y2=map(int,input().split())
+        if abs(x1-y1)==abs(x2-y2):
+            print(1)
+        else:
+            if(x1+y1)%2 != (x2+y2)%2:
+                print("-1")
+            else:
+                print(2)
