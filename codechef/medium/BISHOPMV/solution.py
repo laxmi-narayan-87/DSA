7c81f6ed62c4f38ce2e0ec5d7c143c47b3f29680
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-05 21:36:53 +0530

    ✅ BISHOPMV | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/medium/BISHOPMV/solution.py
+++ codechef/medium/BISHOPMV/solution.py
@@ -3,6 +3,8 @@ if __name__=="__main__":
     t=int(input())
     for _ in range(t):
         x1,y1,x2,y2=map(int,input().split())
+        if (x1==y1) and (x2==y2):
+            print(0)
         if abs(x1-y1)==abs(x2-y2):
             print(1)
         else:
