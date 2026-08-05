56b1dff9b4cf26debd98ca59262352df6d19a89f
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-05 21:41:03 +0530

    ✅ BISHOPMV | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/medium/BISHOPMV/solution.py
+++ codechef/medium/BISHOPMV/solution.py
@@ -3,7 +3,7 @@ if __name__=="__main__":
     t=int(input())
     for _ in range(t):
         x1,y1,x2,y2=map(int,input().split())
-        if abs(x1-x2)==abs(y1-y2):
+        if abs(x1-y1)==abs(x2-y2):
             print(1)
         elif(x1+y1)%2 != (x2+y2)%2:
             print("-1")
