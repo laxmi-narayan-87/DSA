1496e4c178f6a3c9205c4d8646ba5951575a5b12
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-05 10:08:29 +0530

    ✅ FLOW007 - Rating 585 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-05

---
+++ codechef/easy/FLOW007---Rating-585/solution.py
@@ -0,0 +1,9 @@
+# cook your dish here
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(t):
+        x=int(input())
+        if x<=50:
+            print("LEFT")
+        else:
+            print("RIGHT")
