48d96d9b9f2edf798f802667398b25ed64d5052c
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-05 10:43:59 +0530

    ✅ FLOW007 - Rating 588 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/easy/FLOW007---Rating-588/solution.py
+++ codechef/easy/FLOW007---Rating-588/solution.py
@@ -1,1 +1,9 @@
 # cook your dish here
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        rev=0
+        for dig in reversed(str(n)):
+            rev=rev*10 +int(dig)
+        print(rev)
