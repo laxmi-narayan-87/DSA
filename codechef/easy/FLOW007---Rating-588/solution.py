57329ad741e165ff1428dfba9291c9a0662fefec
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-05 10:34:29 +0530

    ✅ FLOW007 - Rating 588 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/easy/FLOW007---Rating-588/solution.py
+++ codechef/easy/FLOW007---Rating-588/solution.py
@@ -1,11 +1,11 @@
 # cook your dish here
-if __name__ =="__main__":
+if __name__=="__main__":
     t=int(input())
     for _ in range(t):
         n=int(input())
         rev=0
         while n>0:
-            dig=n%10
-            rev=rev*10+dig
+            ig=n%10
+            rev=rev*10+ig
             n//=10
         print(rev)
