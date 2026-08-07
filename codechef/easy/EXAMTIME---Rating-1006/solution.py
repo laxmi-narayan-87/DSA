4cd9c9f9a9736d9881261a998bf452e6585bb5b4
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-07 12:36:11 +0530

    ✅ EXAMTIME - Rating 1006 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-07

--- codechef/easy/EXAMTIME---Rating-1006/solution.py
+++ codechef/easy/EXAMTIME---Rating-1006/solution.py
@@ -1,24 +1,39 @@
-# cook your dish here
+# # cook your dish here
+# if __name__=="__main__":
+#     t=int(input())
+#     for _ in range(t):
+#         d1,d2,d3=map(int,input().split())
+#         s1,s2,s3=map(int,input().split())
+#         ds=d1+d2+d3
+#         ss=s1+s2+s3
+#         if (ds>ss):
+#             print("Dragon")
+#         elif(ds<ss):
+#             print("Sloth")
+#         else:
+#             if(d1>s1):
+#                 print("Dragon")
+#             elif(d1<s1):
+#                 print("Sloth")
+#             else:
+#                 if (d2>s2):
+#                     print("Dragon")
+#                 elif (d2<s2):
+#                     print("Sloth")
+#                 else:
+#                     print("TIE")
+                    
+                    
 if __name__=="__main__":
     t=int(input())
     for _ in range(t):
         d1,d2,d3=map(int,input().split())
         s1,s2,s3=map(int,input().split())
-        ds=d1+d2+d3
-        ss=s1+s2+s3
-        if (ds>ss):
-            print("Dragon")
-        elif(ds<ss):
-            print("Sloth")
+        dragon=[d1+d2+d3,d1,d2]
+        sloth=[s1+s2+s3,s1,s2]
+        if dragon>sloth:
+            print("DRAGON")
+        elif dragon<sloth:
+            print("SLOTH")
         else:
-            if(d1>s1):
-                print("Dragon")
-            elif(d1<s1):
-                print("Sloth")
-            else:
-                if (d2>s2):
-                    print("Dragon")
-                elif (d2<s2):
-                    print("Sloth")
-                else:
-                    print("TIE")
+            print("TIE")
