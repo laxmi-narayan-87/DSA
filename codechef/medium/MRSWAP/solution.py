bb3a5c346618e0b24b424a6a66f5af929ecb4625
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-19 21:40:25 +0530

    ✅ MRSWAP | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/MRSWAP/solution.py
+++ codechef/medium/MRSWAP/solution.py
@@ -5,6 +5,6 @@ if __name__=="__main__":
         n=int(input())
         a=list(map(int,input().split()))
         maxsum=0
-        for i in range(n):
+        for i in range(2*n):
             maxsum+= max(a[i],a[2*n-i-1])
-        print(maxsum)
+        print(maxsum//2)
