f38e1e076aa5d22e022ebeb506743118d6555349
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-19 21:54:13 +0530

    ✅ ODDEVEN7 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/ODDEVEN7/solution.py
+++ codechef/medium/ODDEVEN7/solution.py
@@ -1,1 +1,14 @@
 # cook your dish here
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        a=list(map(int,input().split()))
+        ceven=0
+        for num in a:
+            if num%2==0:
+                ceven+=1
+        ans=2*min(ceven,(n-ceven))
+        if ceven!=(n-ceven):
+            ans+=1
+        print(ans)
