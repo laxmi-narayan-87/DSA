0560a3349f5528155ce9adace840b95872324524
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-19 21:59:28 +0530

    ✅ SKIPONE | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/SKIPONE/solution.py
+++ codechef/medium/SKIPONE/solution.py
@@ -1,1 +1,13 @@
 # cook your dish here
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(t):
+        n,k=map(int,input().split())
+        a=list(map(int,input().split()))
+        total=cost=ans=0
+        for i in range(n):
+            total+=a[i]
+            cost=max(cost,a[i])
+            if total-cost<=k:
+                ans=i+1
+        print(ans)
