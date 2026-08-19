7405f420eba79d75e0613c28d55557cb27a57a8c
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-19 21:36:45 +0530

    ✅ MRSWAP | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/MRSWAP/solution.py
+++ codechef/medium/MRSWAP/solution.py
@@ -1,1 +1,10 @@
 # cook your dish here
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        a=list(map(int,input().split()))
+        maxsum=0
+        for i in range(2*n+1):
+            maxsum+= max(a[i],a[2*n-i])
+        print(maxsum)
