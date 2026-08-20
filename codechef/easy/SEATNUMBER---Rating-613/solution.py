ef35863c1231c269c42b28b0807c67af495e57f2
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-20 10:55:21 +0530

    ✅ SEATNUMBER - Rating 613 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-20

--- codechef/easy/SEATNUMBER---Rating-613/solution.py
+++ codechef/easy/SEATNUMBER---Rating-613/solution.py
@@ -2,8 +2,16 @@
 if __name__=="__main__":
     t=int(input())
     for _ in range(t):
-        n,x=map(int,input().split())
-        if x%n==0:
-            print("YES")
+        n=int(input())
+        s1=[1,2,3,4,5,6,7,8,9,10]
+        s2=[11,12,13,14,15]
+        s3=[16,17,18,19,20,21,22,23,24,25]
+        s4=[26,27,28,29,30]
+        if n in s1:
+            print("Lower Double")
+        elif n in s2:
+            print("Lower Single")
+        elif n in s3:
+            print("Upper Double")
         else:
-            print("NO")
+            print("Upper Single")
