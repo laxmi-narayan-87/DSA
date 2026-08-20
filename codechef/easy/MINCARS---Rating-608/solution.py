e2f0d5e08932df86ef11f5f1fc5cc94865fac6d1
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-20 10:17:44 +0530

    ✅ MINCARS - Rating 608 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-20

--- codechef/easy/MINCARS---Rating-608/solution.py
+++ codechef/easy/MINCARS---Rating-608/solution.py
@@ -4,4 +4,8 @@ if __name__=="__main__":
     t=int(input())
     for _ in range(t):
         n=int(input())
-        print(math.ceil(n/4))
+        # print(math.ceil(n/4))
+        if n%4==0:
+            print(n//4)
+        else:
+            print((n//4)+1)
