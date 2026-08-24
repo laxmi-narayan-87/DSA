2efc8292448e9907482bb875f1b4ce6f68a9f270
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-24 21:17:31 +0530

    ✅ FAOCU | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-24

--- codechef/medium/FAOCU/solution.py
+++ codechef/medium/FAOCU/solution.py
@@ -2,3 +2,11 @@
 if __name__=="__main__":
     s=input()
     p=input()
+    k=len(p)
+    tar=sorted(p)
+    res=[]
+    for i in range(len(s)-k+1):
+        ran=s[i:i+k]
+        if sorted(ran)==tar:
+            res.append(i)
+    print(*res)
