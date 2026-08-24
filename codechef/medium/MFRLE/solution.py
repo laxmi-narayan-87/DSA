69bc82c4ce99454a995659dd8482451d0a30aa5e
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-24 20:44:04 +0530

    ✅ MFRLE | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-24

--- codechef/medium/MFRLE/solution.py
+++ codechef/medium/MFRLE/solution.py
@@ -1,1 +1,12 @@
 # cook your dish here
+if __name__=="__main__":
+    s=input()
+    freq={}
+    for x in s:
+        if x.isalpha():
+            if x not in freq:
+                freq[x]=1
+            else:
+                freq[x]+=1
+    max_x=max(freq,key=freq.get)
+    print(max_x)
