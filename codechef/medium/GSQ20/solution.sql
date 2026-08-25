e9fd4fe16f4193d3c80d61da46273eb2fd1c779b
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-25 12:48:28 +0530

    ✅ GSQ20 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-25

---
+++ codechef/medium/GSQ20/solution.sql
@@ -0,0 +1,6 @@
+/* Write a query to do the following
+- Output all entries from the table
+- Where passenger_name between 'A' and 'D' */
+
+SELECT * FROM Flights 
+WHERE passenger_name BETWEEN 'A' AND 'D';
