6359f5aaec6c4cb38df672426b3d839a599ec745
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-25 11:28:49 +0530

    ✅ CANDYDIST - Rating 668 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-25

--- codechef/easy/CANDYDIST---Rating-668/solution.py
+++ codechef/easy/CANDYDIST---Rating-668/solution.py
@@ -2,5 +2,8 @@
 if __name__=="__main__":
     t=int(input())
     for _ in range(t):
-        n,x,k=map(int,input().split())
-        print(min(n,(k//x)))
+        n,m=map(int,input().split())
+        if n%m==0 and (n//m) %2==0:
+            print("Yes")
+        else:
+            print("NO")
