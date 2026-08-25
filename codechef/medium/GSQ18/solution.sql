39c3a5971f36f56ddfd8908c8ca4e2e7beed6b25
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-25 12:44:33 +0530

    ✅ GSQ18 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-25

--- codechef/medium/GSQ18/solution.sql
+++ codechef/medium/GSQ18/solution.sql
@@ -1,2 +1,4 @@
-/* Write a query to find the unique origin locations */
-SELECT DISTINCT Origin FROM Flights;
+/* Write a query which does the following
+- Fetch the entry specifically from 2 columns - 'Passenger_name' and 'Gender' */
+
+SELECT Passenger_name, Gender FROM Flights;
