686e2ac223591838ca22d397ef5a5ee4731fc65c
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-27 10:06:22 +0530

    ✅ RSMSPR06A | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-27

---
+++ codechef/medium/RSMSPR06A/solution.sql
@@ -0,0 +1,11 @@
+BEGIN TRANSACTION;
+SAVEPOINT S1;
+--Savepoint created incase any changes to the the database beyond this point has to be undone in the future.
+
+/* Update your query below this line*/
+
+DELETE FROM Orders
+WHERE total_amount< 150;
+
+SELECT order_id,customer_id, total_amount
+FROM Orders;
