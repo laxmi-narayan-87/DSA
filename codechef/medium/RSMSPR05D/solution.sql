cf4bc539971093944b6bf75f8a3b447784e8f4e5
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-27 10:06:20 +0530

    ✅ RSMSPR05D | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-27

--- codechef/medium/RSMSPR05D/solution.sql
+++ codechef/medium/RSMSPR05D/solution.sql
@@ -2,22 +2,10 @@ BEGIN TRANSACTION;
 SAVEPOINT S1;
 --Savepoint created incase any changes to the the database beyond this point has to be undone in the future.
 
+/* Update your query below this line*/
+
 DELETE FROM Orders
-WHERE order_date < '2024-01-20';
+WHERE total_amount< 150;
 
-/* Update your query here */
-
--- DELETE c 
--- FROM Customers c
--- LEFT JOIN Orders o
--- on c.customer_id= o.customer_id
--- WHERE o.order_id IS NULL;
-
-DELETE FROM Customers 
-WHERE NOT EXISTS(
-SELECT 1 
-FROM Orders o 
-WHERE o.customer_id=Customers.customer_id);
-
-SELECT customer_id, name 
-FROM Customers;
+SELECT order_id,customer_id, total_amount
+FROM Orders;
