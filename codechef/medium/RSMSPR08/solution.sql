0256b3d095d942b066da141a866d61b4ac2b2e1c
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-27 10:51:16 +0530

    ✅ RSMSPR08 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-27

--- codechef/medium/RSMSPR08/solution.sql
+++ codechef/medium/RSMSPR08/solution.sql
@@ -1,8 +1,24 @@
-/* Update your query here*/
+/* Write all the 5 queries here and RUN/SUBMIT them all at once*/
 
-UPDATE Orders 
-SET Remarks_if_any = 'NULL'
-WHERE Remarks_if_any='No Remarks';
+SELECT order_id,customer_id,order_date
+FROM Orders 
+WHERE order_date BETWEEN '2024-01-01' AND '2024-01-31';
 
-SELECT order_id,Remarks_if_any
-FROM Orders WHERE Remarks_if_any= 'NULL';
+SELECT max(order_date) as most_recent_order
+FROM Orders;
+
+SELECT order_date, COUNT(*) as order_count 
+FROM Orders
+WHERE order_date BETWEEN '2024-01-15' AND '2024-01-17'
+GROUP BY order_date;
+
+
+SELECT JULIANDAY(MAX(order_date)) - JULIANDAY(MIN(order_date)) as days_between
+FROM Orders;
+
+
+
+SELECT order_id,customer_id,order_date, total_amount
+FROM Orders 
+WHERE order_date BETWEEN DATE('2024-01-24', '-5 days')
+                     AND DATE('2024-01-24', '-1 day');
