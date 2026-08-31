1d75a4b267b0f8acb237f9a55b4238b5f8d939d2
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-31 11:07:17 +0530

    ✅ ASQL01B | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/ASQL01B/solution.sql
+++ codechef/medium/ASQL01B/solution.sql
@@ -1,9 +1,6 @@
-/* Write the queries to do the following:
- - JOIN the tables 'student' and 'course' using 'Course_id' to match both the tables and output the joined table.
- - RIGHT JOIN the tables 'student' and 'course' using 'Course_id' to match both the tables and output the joined table. */
+-- Write a query to do the following:
+
+-- FULL OUTER JOIN the 'student' and 'course' tables using 'Course_id' to match the tables. Output the joined table.
+
  
- 
- 
- SELECT * FROM student s JOIN course c on s.Course_id=c.Course_id;
- 
- SELECT * FROM student s RIGHT JOIN course c on s.Course_id=c.Course_id;
+ SELECT * FROM student s FULL OUTER JOIN course c on s.Course_id=c.Course_id;
