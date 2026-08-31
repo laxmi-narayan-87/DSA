a5dc6c7c2c6b6689c1088105b201d7f11000cbab
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-31 11:04:46 +0530

    ✅ GSQ63 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/GSQ63/solution.sql
+++ codechef/medium/GSQ63/solution.sql
@@ -1,3 +1,7 @@
-/* Write a query to join the tables 'student' and 'course' and output the same. Check if you can find the course with id ENG201 in the output */
-
-SELECT * FROM student st INNER JOIN course c ON st.Course_id = c.Course_id;
+/* Write a query to do the following:
+ - JOIN the tables 'student' and 'course' using 'Course_id' to match both the tables and output the joined table.
+ - LEFT JOIN the tables 'student' and 'course' using 'Course_id' to match both the tables and output the joined table. */
+ 
+SELECT * FROM student s JOIN course c on s.Course_id=c.Course_id;
+ 
+SELECT * FROM student s LEFT JOIN course c on s.Course_id=c.Course_id;
