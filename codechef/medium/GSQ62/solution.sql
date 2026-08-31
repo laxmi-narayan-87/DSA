2cafe974aac519552c5a0d2eb0f2c7ce99b82655
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-08-31 11:01:15 +0530

    ✅ GSQ62 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/GSQ62/solution.sql
+++ codechef/medium/GSQ62/solution.sql
@@ -1,3 +1,3 @@
-/* Write a query to join the table 'student' and 'course' using 'Course_id' to match both the tables and output the joined table. */
+/* Write a query to join the tables 'student' and 'course' and output the same. Check if you can find the course with id ENG201 in the output */
 
-SELECT * FROM student JOIN course on  student.course_ID = course.course_id;
+SELECT * FROM student st INNER JOIN course c ON st.Course_id = c.Course_id;
