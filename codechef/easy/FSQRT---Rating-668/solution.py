fc72911f73df176223914c9e36317b36790a1ca7
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-09-03 18:19:41 +0530

    ✅ FSQRT - Rating 668 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-03

--- codechef/easy/FSQRT---Rating-668/solution.py
+++ codechef/easy/FSQRT---Rating-668/solution.py
@@ -2,8 +2,5 @@
 if __name__=="__main__":
     t=int(input())
     for _ in range(t):
-        n,m=map(int,input().split())
-        if n%m==0 and (n//m) %2==0:
-            print("Yes")
-        else:
-            print("NO")
+        n=int(input())
+        print(int(n**(0.5)))
