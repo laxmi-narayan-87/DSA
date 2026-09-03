de3bb9d33a52b38b4c34e8aa1a346c44064d2fe8
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-09-03 18:37:38 +0530

    ✅ FILLCANDIES - Rating 681 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-03

--- codechef/easy/FILLCANDIES---Rating-681/solution.py
+++ codechef/easy/FILLCANDIES---Rating-681/solution.py
@@ -1,10 +1,7 @@
 # cook your dish here
+import math
 if __name__=="__main__":
     t=int(input())
     for _ in range(t):
-        a,b=map(int,input().split())
-        req=21-(a+b)
-        if 1<=req<=10:
-            print(req)
-        else:
-            print("-1")
+        n,k,m=map(int,input().split())
+        print(math.ceil(n/(k*m)))
