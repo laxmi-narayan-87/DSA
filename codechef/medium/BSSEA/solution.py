98692d4990cc5803873d9fa53720c34a127f118f
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-09-15 09:25:57 +0530

    ✅ BSSEA | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-14

--- codechef/medium/BSSEA/solution.py
+++ codechef/medium/BSSEA/solution.py
@@ -5,6 +5,6 @@ if __name__=="__main__":
     avg=(a[0]+a[-1])/2
     close=a[0]
     for x in a:
-        if (abs(x-avg)<abs(close-avg)) or (abs(x-avg)==abs(close-avg) and x<close):
+        if abs(x-avg)<abs(close-avg):
             close=x
     print(close)
