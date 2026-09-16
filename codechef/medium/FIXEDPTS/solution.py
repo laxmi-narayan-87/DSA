989529ca7ba7a07ff4ab4aef45c1722d9cefd3a5
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-09-16 21:12:18 +0530

    ✅ FIXEDPTS | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/FIXEDPTS/solution.py
+++ codechef/medium/FIXEDPTS/solution.py
@@ -1,9 +1,1 @@
 # cook your dish here
-if __name__=="__main__":
-    t=int(input())
-    for _ in range(t):
-        n,k=map(int,input().split())
-        if k==n-1:
-            print("NO")
-        else:
-            print("YES")
