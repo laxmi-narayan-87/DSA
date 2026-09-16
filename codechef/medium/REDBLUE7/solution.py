ca7f09b3aea7cc1e6a5e807fe9d1248f31e5f784
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-09-16 21:27:05 +0530

    ✅ REDBLUE7 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ codechef/medium/REDBLUE7/solution.py
@@ -0,0 +1,14 @@
+# cook your dish here
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        a=list(map(int,input().split()))
+        # a.sort()
+        sr=0
+        maxvalue=0
+        for k in range(1,n//2+1):
+            sr+=a[n-k]
+            value=sr*(n-2*k)+n*k
+            maxvalue=max(value,maxvalue)
+        print(maxvalue)
