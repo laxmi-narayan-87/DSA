0ebbbb1b2427ba7f1566c4c52e5911d99c7e115d
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-09-16 21:48:26 +0530

    ✅ FARSWAPLEX | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/FARSWAPLEX/solution.py
+++ codechef/medium/FARSWAPLEX/solution.py
@@ -4,4 +4,15 @@ if __name__=="__main__":
     for _ in range(t):
         n=int(input())
         p=list(map(int,input().split()))
+        # if n<=3:
+        #     print(*p)
+        # else:
+        #     print("2 1 3 5 4")
+        change=True
+        while change:
+            change=False
+            for i in range(n-1):
+                if p[i]>p[i+1] and abs(p[i]-p[i+1])>1:
+                    p[i],p[i+1]=p[i+1],p[i]
+                    change=True
         print(*p)
