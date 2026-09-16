6e32bddf66bacf031f0b0671c4b5c68ba3a8b8eb
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-09-16 21:01:01 +0530

    ✅ BUSSEAT | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/BUSSEAT/solution.py
+++ codechef/medium/BUSSEAT/solution.py
@@ -1,1 +1,9 @@
 # cook your dish here
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(t):
+        n,k=map(int,input().split())
+        if k>n:
+            print((k-n)*2)
+        else:
+            print("0")
