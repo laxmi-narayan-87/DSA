a2a2cf9edb881045e3728a8a231df8efa3c89258
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2026-09-17 09:34:18 +0530

    ✅ FARSWAPLEX | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/FARSWAPLEX/solution.py
+++ codechef/medium/FARSWAPLEX/solution.py
@@ -4,10 +4,6 @@ if __name__=="__main__":
     for _ in range(t):
         n=int(input())
         p=list(map(int,input().split()))
-        # if n<=3:
-        #     print(*p)
-        # else:
-        #     print("2 1 3 5 4")
         change=True
         while change:
             change=False
